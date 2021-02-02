8c3ea0ea48f6fbf4aed8bf14244f492dd5cb07cb
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-02-02 20:14:40 +0600

    Scope, block scope, access outer scope variable

---
+++ scope.js
@@ -0,0 +1,52 @@
+// NUMBER O1:
+function sum(first, second){
+    let result = first + second;
+    console.log(result);
+    return result;
+}
+
+const output = sum(3, 7);
+
+console.log(output);
+
+
+// NUMBER 02:
+let bonus = 20;
+
+function sum2(first, second){
+    let result2 = first + second + bonus;
+    console.log(bonus);
+    return result2;
+}
+
+const output2 = sum2(3, 7);
+console.log(bonus);
+console.log(output2);
+
+
+
+// NUMBER 03:
+let bonus2 = 20;
+
+function sum3(first, second){
+    let result3 = first + second + bonus2;
+    // console.log(bonus2);
+    if(result3 > 9){
+        let mood = 'happy';
+        mood = 'fishy';
+        mood = 'funky';
+        mood = 'cranky';
+        console.log(mood);
+    }
+    console.log(mood);
+    return result3;
+}
+
+const output3 = sum3(3, 7);
+// console.log(bonus2);
+// console.log(output3);
+
+// NUMBER 04:
+console.log(day);
+var day = 'friday'
+console.log(day);
