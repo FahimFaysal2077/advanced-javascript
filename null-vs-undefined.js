82a78cf49cdcb17b5e08993eecff532df82174d8
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-02-02 11:01:39 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,40 @@
+// NUMBER: 01
+let pakhi;
+console.log(pakhi);
+
+// NUMBER: 02
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+const result = add(13, 82);
+console.log(result);
+
+// NUMBER: 03
+function add2(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+const result2 = add2(10, 22);
+console.log(result2);
+
+// NUMBER: 04
+function add3(num1, num2) {
+    console.log(num1, num2);
+}
+
+const result3 = add3(15);
+console.log(result3);
+
+// NUMBER: 05
+const premik = { name: 'Smart Dude', phone: 458421 };
+console.log(premik.gf);
+
+// NUMBER: 06
+let fun = undefined;
+console.log(fun);
+
+// NUMBER: 07
+let ages = [2, 5, 9];
+console.log(ages[11]);
