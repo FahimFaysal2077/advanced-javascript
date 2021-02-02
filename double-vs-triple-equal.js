09774f3679c69454b759fb4bc0fb29d9f803caa1
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-02-02 11:56:20 +0600

    double-vs-triple-equal

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,83 +1,94 @@
 // (==) <---- double equal mane hocha sudho value ta ke check korbe.
 const first = 2;
 const second = 2;
-if(first == second){
+if (first == second) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 const first2 = 2;
 const second2 = '2';
-if(first2 == second2){
+if (first2 == second2) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 
 const first4 = 2;
 const second4 = true;
-if(first4 == second4){
+if (first4 == second4) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 // 1 = true.
 const first5 = 1;
 const second5 = true;
-if(first5 == second5){
+if (first5 == second5) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 
 const first6 = 1;
 const second6 = false;
-if(first6 == second6){
+if (first6 == second6) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 // 0 = false.
 const first7 = 0;
 const second7 = false;
-if(first7 == second7){
+if (first7 == second7) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 
 
 
- 
+
 // (===) <---- triple equal mane hocha value and type check korbe.
 // {MOST USE} //
 const first3 = 2;
 const second3 = '2';
-if(first3 === second3){
+if (first3 === second3) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
 }
 
 
 const first8 = 0;
 const second8 = false;
-if(first8 === second8){
+if (first8 === second8) {
     console.log('condition is true');
 }
-else{
+else {
     console.log('condition is false');
+}
+
+
+
+
+if ('2'=== 2) {
+    console.log('Inside if');
+} 
+    else {
+    console.log('Inside else');
+
 }
